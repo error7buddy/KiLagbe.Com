443e8038acd42d45cc33c31758c7c1b3aa202457
Osman <fxosman7@gmail.com.com>
2026-01-24 16:22:20 +0600

    Analytics

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom"
 import Navbar from "./Navbar/Navbar";
 import Footer from "./Footer/Footer";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
